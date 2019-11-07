08104b7a51a41941f2fb261936634484e492c8ed
Sitthiphong Muntrima <sitthiphong.mun@gmail.com>
2019-11-07 16:40:59 +0700

    - Update login ui - Fig login with email

--- app/src/main/java/com/think/runex/java/Activities/LoginActivity.java
+++ app/src/main/java/com/think/runex/java/Activities/LoginActivity.java
@@ -3,15 +3,22 @@ package com.think.runex.java.Activities;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.FrameLayout;
+import android.widget.RelativeLayout;
 import android.widget.Toast;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.AppCompatButton;
+import androidx.appcompat.widget.AppCompatImageButton;
+import androidx.appcompat.widget.AppCompatImageView;
 import androidx.fragment.app.FragmentActivity;
 
 import com.facebook.internal.CallbackManagerImpl;
+import com.google.android.material.textview.MaterialTextView;
 import com.think.runex.R;
 import com.think.runex.feature.social.SocialLoginListener;
 import com.think.runex.feature.social.SocialLoginManger;
@@ -51,84 +58,28 @@ public class LoginActivity extends FragmentActivity implements
 
     // instance variables
     private SocialLoginManger socialLoginManger;
-    private ActivityUtils activityUtils;
 
-    // views
-    private View btnLogin;
-    private View btnLoginWithFacebook;
-    private View btnLoginWithGoogle;
+    //Layout
+    private RelativeLayout loginLayout;
+    private RelativeLayout loginEmailLayout;
+
+    // Button
+    private AppCompatButton btnLogin;
+    private FrameLayout btnLoginEmail;
+    private FrameLayout btnLoginFacebook;
+    private FrameLayout btnLoginGoogle;
+    private MaterialTextView btnRegister;
+    private MaterialTextView btnRegister2;
+    private AppCompatImageButton btnClose;
+
     //--> Input fields
     private EditText inputEmail;
     private EditText inputPassword;
-
-    /**
-     * View on click
-     */
-    @Override
-    public void onClick(View view) {
-        // prepare usage variables
-        final List<String> permissions = new ArrayList<>();
-
-        // update
-        permissions.add("email");
-
-        switch (view.getId()) {
-            case R.id.btn_login:
-                loginWithEmail();
-                break;
-            case R.id.btn_login_with_google:
-                socialLoginManger.loginWithGoogle(this);
-                break;
-            case R.id.btn_login_with_facebook:
-                socialLoginManger.loginWithFacebook(this, permissions);
-                break;
-        }
-
-    }
-
-    /**
-     * Implement methods
-     */
-    @Override
-    public void onLoginWithSocialCompleted(int platform, @NotNull UserProvider userProvider) {
-        // prepare usage variables
-        final String mtn = ct + "onLoginWithSocialCompleted() ";
-        final rqSocialPd request = getRqSocialPd(platform, userProvider);
-
-        L.i(mtn + " * * * Social Login Completed * * * ");
-        L.i(mtn + "platform: " + SocialPlatform.Companion.platformText(platform));
-        L.i(mtn + "user's provider: " + Globals.GSON.toJson(userProvider));
-        L.i(mtn + "request PD: " + Globals.GSON.toJson(request));
-
-        // request token
-        apiSocialsLogin(request);
-
-    }
-
-    @Override
-    public void onLoginWithSocialCancel() {
-
-    }
-
-    @Override
-    public void onLoginWithSocialError(@NotNull Exception exception) {
-
-    }
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
-
-        // Activity utils
-        ActivityUtils actUtls = ActivityUtils.newInstance(this);
-        actUtls.activity = this;
-        actUtls.fullScreen();
-
-        // activity manager
-        activityUtils = ActivityUtils.newInstance(this);
-        activityUtils.activity = this;
-        activityUtils.fullScreen();
 
         //Initial social login manage.
         socialLoginManger = new SocialLoginManger();
@@ -147,6 +98,113 @@ public class LoginActivity extends FragmentActivity implements
     }
 
     /**
+     * Matching views
+     */
+    private void matchingViews() {
+        loginLayout = findViewById(R.id.login_layout);
+        loginEmailLayout = findViewById(R.id.login_email_layout);
+
+
+        btnClose = findViewById(R.id.btn_close);
+        btnLogin = findViewById(R.id.btn_login_as_email);
+        btnLoginEmail = findViewById(R.id.btn_login_with_email);
+        btnLoginFacebook = findViewById(R.id.btn_login_with_facebook);
+        btnLoginGoogle = findViewById(R.id.btn_login_with_google);
+        btnRegister = findViewById(R.id.btn_register);
+        btnRegister2 = findViewById(R.id.btn_register_2);
+
+        //--> Edit text
+        inputEmail = findViewById(R.id.edt_email);
+        inputPassword = findViewById(R.id.edt_password);
+    }
+
+    /**
+     * View event listener
+     */
+    private void viewEventListener() {
+        btnClose.setOnClickListener(this);
+        btnLogin.setOnClickListener(this);
+        btnLoginEmail.setOnClickListener(this);
+        btnLoginFacebook.setOnClickListener(this);
+        btnLoginGoogle.setOnClickListener(this);
+        btnRegister.setOnClickListener(this);
+        btnRegister2.setOnClickListener(this);
+    }
+
+
+    /**
+     * View on click
+     */
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()) {
+            case R.id.btn_close:
+                if (loginEmailLayout.getVisibility() == View.VISIBLE) {
+                    loginLayout.setVisibility(View.VISIBLE);
+                    loginEmailLayout.setVisibility(View.GONE);
+                } else {
+                    // set activity result
+                    setResult(Activity.RESULT_CANCELED);
+                    // exit from this process
+                    finish();
+                }
+                break;
+            case R.id.btn_login_with_facebook:
+                // prepare usage variables
+                final List<String> permissions = new ArrayList<>();
+                // update
+                permissions.add("email");
+                socialLoginManger.loginWithFacebook(this, permissions);
+                break;
+            case R.id.btn_login_with_google:
+                socialLoginManger.loginWithGoogle(this);
+                break;
+            case R.id.btn_login_with_email:
+                loginLayout.setVisibility(View.GONE);
+                loginEmailLayout.setVisibility(View.VISIBLE);
+                break;
+            case R.id.btn_login_as_email:
+                loginWithEmail();
+                break;
+            case R.id.btn_register:
+
+                break;
+            case R.id.btn_register_2:
+
+                break;
+
+        }
+    }
+
+    /**
+     * Implement methods
+     */
+    @Override
+    public void onLoginWithSocialCompleted(int platform, @NotNull UserProvider userProvider) {
+        // prepare usage variables
+        final String mtn = ct + "onLoginWithSocialCompleted() ";
+        final rqSocialPd request = getRqSocialPd(platform, userProvider);
+
+        L.i(mtn + " * * * Social Login Completed * * * ");
+        L.i(mtn + "platform: " + SocialPlatform.Companion.platformText(platform));
+        L.i(mtn + "user's provider: " + Globals.GSON.toJson(userProvider));
+        L.i(mtn + "request PD: " + Globals.GSON.toJson(request));
+
+        // request token
+        apiSocialsLogin(request);
+    }
+
+    @Override
+    public void onLoginWithSocialCancel() {
+
+    }
+
+    @Override
+    public void onLoginWithSocialError(@NotNull Exception exception) {
+
+    }
+
+    /**
      * Feature methods
      */
 
@@ -155,17 +213,21 @@ public class LoginActivity extends FragmentActivity implements
 
     }
 
-    private boolean validate() {
-        return true;
-    }
 
     private void loginWithEmail() {
-        if (validate()) {
-            apiLogin(new rqLogin(inputEmail.getText().toString(),
-                    inputPassword.getText().toString(),
-                    Globals.PLATFORM));
+        String email = inputEmail.getText().toString();
+        String password = inputPassword.getText().toString();
 
+        if (email.length() == 0) {
+            Toast.makeText(this, R.string.email_required, Toast.LENGTH_SHORT).show();
+            return;
         }
+        if (password.length() == 0) {
+            Toast.makeText(this, R.string.password_required, Toast.LENGTH_SHORT).show();
+            return;
+        }
+
+        apiLogin(new rqLogin(email, password, Globals.PLATFORM));
     }
 
     /**
@@ -275,6 +337,7 @@ public class LoginActivity extends FragmentActivity implements
     }
 
     private void apiLogin(rqLogin request) {
+        Log.e("Jozzee", "apiLogin");
         // prepare usage variables
         final String mtn = ct + "apiLogin() ";
         NetworkUtils nw = NetworkUtils.newInstance(this);
@@ -291,30 +354,27 @@ public class LoginActivity extends FragmentActivity implements
                 try {
                     // prepare usage variables
                     final String jsonString = rsp.jsonString;
-                    final AppEntity appEntity = App.instance(LoginActivity.this).getAppEntity();
                     final TokenObject token = Globals.GSON.fromJson(jsonString, TokenObject.class);
-                    L.i(mtn + "onSuccess: " + jsonString);
 
-                    // update token
-                    appEntity.setToken(token);
-                    //--> confirm user has logged in
-                    appEntity.setLoggedIn(true);
+                    if (token.isSuccessFul()) {
+                        final AppEntity appEntity = App.instance(LoginActivity.this).getAppEntity();
+                        // update token
+                        appEntity.setToken(token);
+                        //--> confirm user has logged in
+                        appEntity.setLoggedIn(true);
 
-                    // commit
-                    updateAppEntity(appEntity);
+                        // commit
+                        updateAppEntity(appEntity);
 
-                    // call user profile
-                    apiGetProfile();
+                        // call user profile
+                        apiGetProfile();
+                    } else {
+                        Toast.makeText(LoginActivity.this, token.getMessage(), Toast.LENGTH_SHORT).show();
+                    }
 
                 } catch (Exception e) {
                     L.e(mtn + "Err: " + e);
-
-                    // set activity result
-                    setResult(Activity.RESULT_CANCELED);
-
-                    // exit from this process
-                    finish();
-
+                    Toast.makeText(LoginActivity.this, e.getMessage(), Toast.LENGTH_SHORT).show();
                 }
 
             }
@@ -359,29 +419,6 @@ public class LoginActivity extends FragmentActivity implements
         } else L.e(mtn + "Platform does not match.");
 
         return rq;
-    }
-
-    /**
-     * View event listener
-     */
-    private void viewEventListener() {
-        btnLogin.setOnClickListener(this);
-        btnLoginWithFacebook.setOnClickListener(this);
-        btnLoginWithGoogle.setOnClickListener(this);
-
-    }
-
-    /**
-     * Matching views
-     */
-    private void matchingViews() {
-        btnLogin = findViewById(R.id.btn_login);
-        btnLoginWithFacebook = findViewById(R.id.btn_login_with_facebook);
-        btnLoginWithGoogle = findViewById(R.id.btn_login_with_google);
-
-        //--> Edit text
-        inputEmail = findViewById(R.id.edt_email);
-        inputPassword = findViewById(R.id.edt_password);
     }
 
     @Override
