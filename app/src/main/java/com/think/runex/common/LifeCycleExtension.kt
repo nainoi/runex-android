1423b982685b3701827ea7a400390de065ad34fb
Sitthiphong Muntrima <sitthiphong.mun@gmail.com>
2020-11-04 11:16:07 +0700

    Update Extension

---
+++ app/src/main/java/com/think/runex/common/LifeCycleExtension.kt
@@ -0,0 +1,28 @@
+package com.think.runex.common
+
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentActivity
+import androidx.lifecycle.LifecycleOwner
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+
+
+fun <T : Any, L : LiveData<T>> LifecycleOwner.observe(liveData: L, body: (T?) -> Unit) {
+    liveData.observe(this, Observer(body))
+}
+
+fun <T : Any, L : LiveData<T>> LifecycleOwner.removeObservers(liveData: L) {
+    liveData.removeObservers(this)
+}
+
+inline fun <reified T : ViewModel> FragmentActivity.getViewModel(viewModelFactory: ViewModelProvider.Factory? = null): T {
+    return viewModelFactory?.let { ViewModelProvider(this, it).get(T::class.java) }
+            ?: ViewModelProvider(this).get(T::class.java)
+}
+
+inline fun <reified T : ViewModel> Fragment.getViewModel(viewModelFactory: ViewModelProvider.Factory? = null): T {
+    return viewModelFactory?.let { ViewModelProvider(this, it).get(T::class.java) }
+            ?: ViewModelProvider(this).get(T::class.java)
+}
