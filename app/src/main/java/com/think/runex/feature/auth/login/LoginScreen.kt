42ce4d36f34aca0f243cbacf29bffaa3ac3ac73a
Sitthiphong Muntrima <sitthiphong.mun@gmail.com>
2021-04-19 03:57:15 +0700

    Update login on Android 6

--- app/src/main/java/com/think/runex/feature/auth/login/LoginScreen.kt
+++ app/src/main/java/com/think/runex/feature/auth/login/LoginScreen.kt
@@ -5,6 +5,8 @@ import android.app.AlarmManager
 import android.app.PendingIntent
 import android.content.Context
 import android.content.Intent
+import android.net.Uri
+import android.os.Build
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
@@ -14,26 +16,28 @@ import com.jozzee.android.core.resource.getColor
 import com.jozzee.android.core.view.*
 import com.think.runex.BuildConfig
 import com.think.runex.R
-import com.think.runex.util.extension.fadeIn
-import com.think.runex.util.extension.getViewModel
-import com.think.runex.util.extension.setStatusBarColor
-import com.think.runex.feature.auth.AuthViewModel
 import com.think.runex.base.BaseScreen
 import com.think.runex.config.FACE_USER_AGENT_FOR_WEB_VIEW
 import com.think.runex.config.RC_RESTART_APP
 import com.think.runex.datasource.api.ApiConfig
+import com.think.runex.feature.auth.AuthViewModel
 import com.think.runex.feature.main.MainScreen
 import com.think.runex.feature.setting.EnvironmentDialog
 import com.think.runex.util.AppPreference
+import com.think.runex.util.extension.fadeIn
+import com.think.runex.util.extension.getViewModel
 import com.think.runex.util.extension.launch
+import com.think.runex.util.extension.setStatusBarColor
 import kotlinx.android.synthetic.main.screen_login.*
 import kotlinx.coroutines.delay
 import kotlin.system.exitProcess
+
 
 class LoginScreen : BaseScreen(), EnvironmentDialog.OnEnvironmentSelectedListener {
 
     private lateinit var viewModel: AuthViewModel
 
+    //private var webViewPopup: WebView? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -56,39 +60,75 @@ class LoginScreen : BaseScreen(), EnvironmentDialog.OnEnvironmentSelectedListene
     private fun setupComponents() {
         setStatusBarColor(getColor(R.color.backgroundPrimaryLight), isLightStatusBar = true)
 
+        set_environment_button?.setVisible(BuildConfig.DEBUG)
+
         web_view?.settings?.apply {
+            javaScriptEnabled = true
+            setSupportMultipleWindows(true)
             //setAppCacheEnabled(false)
             cacheMode = WebSettings.LOAD_NO_CACHE
-            javaScriptEnabled = true
             //Set face user agent for google sign in.
             userAgentString = FACE_USER_AGENT_FOR_WEB_VIEW
         }
-        web_view.webViewClient = object : WebViewClient() {
+
+        CookieManager.getInstance().setAcceptThirdPartyCookies(web_view, true)
+        //Cookie manager for the webview
+        val cookieManager = CookieManager.getInstance()
+        cookieManager.setAcceptCookie(true)
+
+        web_view?.webViewClient = object : WebViewClient() {
             override fun onPageFinished(view: WebView?, url: String?) {
                 super.onPageFinished(view, url)
+                //Log.i("Jozzee", "onPageFinished: $url")
                 progress_bar?.gone()
-                set_environment_button?.setVisible(BuildConfig.DEBUG)
+
+                if (Build.VERSION.SDK_INT <= Build.VERSION_CODES.M) {
+
+                    val uri = Uri.parse(url)
+
+                    if (uri?.scheme == BuildConfig.APP_SCHEME) {
+                        val parameters = uri.getQueryParameters("code")
+                        if (parameters?.isNotEmpty() == true) {
+                            performLogin(parameters[0])
+                            set_environment_button?.gone()
+                        }
+                    }
+                }
             }
 
+            //@RequiresApi(Build.VERSION_CODES.N)
             override fun shouldOverrideUrlLoading(view: WebView?, request: WebResourceRequest?): Boolean {
 
-                val url = request?.url
+                val uri = request?.url
 
-                if (url?.scheme == BuildConfig.APP_SCHEME) {
-                    val parameters = request.url?.getQueryParameters("code")
+                //Log.w("Jozzee", "shouldOverrideUrlLoading: $uri")
+                //Log.w("Jozzee", "shouldOverrideUrlLoading Host: ${uri?.host}")
+
+                if (uri?.scheme == BuildConfig.APP_SCHEME) {
+                    val parameters = uri.getQueryParameters("code")
                     if (parameters?.isNotEmpty() == true) {
                         performLogin(parameters[0])
                         set_environment_button?.gone()
                     }
-                } else if (url?.scheme == "auth.runex.co") {
+                } else if (uri?.scheme == "auth.runex.co") {
                     web_view?.inVisible()
                     set_environment_button?.gone()
                 }
 
-                return when (url?.toString()?.startsWith("http") == true || url?.toString()?.startsWith("https") == true) {
+                if (Build.VERSION.SDK_INT == Build.VERSION_CODES.N && uri?.host == "m.facebook.com") {
+                    web_view?.loadUrl(uri.toString())
+                    return false
+                }
+
+                return when (uri?.toString()?.startsWith("http") == true || uri?.toString()?.startsWith("https") == true) {
                     true -> super.shouldOverrideUrlLoading(view, request)
                     false -> true
                 }
+            }
+
+            override fun onReceivedError(view: WebView?, request: WebResourceRequest?, error: WebResourceError?) {
+                super.onReceivedError(view, request, error)
+                //Log.e("Jozzee", "Error: ${error?.description}")
             }
         }
         web_view.loadUrl("${ApiConfig.LOGIN_URL}?device=android")
@@ -157,4 +197,98 @@ class LoginScreen : BaseScreen(), EnvironmentDialog.OnEnvironmentSelectedListene
         CookieManager.getInstance().flush()
         super.onDestroy()
     }
+
+//    inner class UriWebViewClient : WebViewClient() {
+//        override fun onPageFinished(view: WebView?, url: String?) {
+//            super.onPageFinished(view, url)
+//            Log.i("Jozzee", "onPageFinished: $url")
+//            progress_bar?.gone()
+//
+//            if (Build.VERSION.SDK_INT <= Build.VERSION_CODES.M) {
+//
+//                val uri = Uri.parse(url)
+//
+//                if (uri?.scheme == BuildConfig.APP_SCHEME) {
+//                    val parameters = uri.getQueryParameters("code")
+//                    if (parameters?.isNotEmpty() == true) {
+//                        performLogin(parameters[0])
+//                        set_environment_button?.gone()
+//                    }
+//                }
+//            }
+//        }
+//
+//        //@RequiresApi(Build.VERSION_CODES.N)
+//        override fun shouldOverrideUrlLoading(view: WebView?, request: WebResourceRequest?): Boolean {
+//
+//            val uri = request?.url
+//
+//            Log.w("Jozzee", "shouldOverrideUrlLoading: $uri")
+//            Log.w("Jozzee", "shouldOverrideUrlLoading Host: ${uri?.host}")
+//
+//            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {
+//                Log.w("Jozzee", "is isRedirect: ${request?.isRedirect}")
+//            }
+//
+//            if (uri?.scheme == BuildConfig.APP_SCHEME) {
+//                val parameters = uri.getQueryParameters("code")
+//                if (parameters?.isNotEmpty() == true) {
+//                    performLogin(parameters[0])
+//                    set_environment_button?.gone()
+//                }
+//            } else if (uri?.scheme == "auth.runex.co") {
+//                web_view?.inVisible()
+//                set_environment_button?.gone()
+//            }
+//
+//            if (webViewPopup != null) {
+//                webViewPopup?.gone()
+//                web_view_popup_container?.removeAllViews()
+//                webViewPopup = null
+//            }
+//
+//            if (Build.VERSION.SDK_INT == Build.VERSION_CODES.N && uri?.host == "m.facebook.com") {
+//                return false
+//            }
+//
+//            return when (uri?.toString()?.startsWith("http") == true || uri?.toString()?.startsWith("https") == true) {
+//                true -> super.shouldOverrideUrlLoading(view, request)
+//                false -> true
+//            }
+//        }
+//
+//        override fun onReceivedError(view: WebView?, request: WebResourceRequest?, error: WebResourceError?) {
+//            super.onReceivedError(view, request, error)
+//            Log.e("Jozzee", "Error: ${error?.description}")
+//        }
+//    }
+//
+//    inner class UriChromeClient : WebChromeClient() {
+//
+//        @SuppressLint("SetJavaScriptEnabled")
+//        override fun onCreateWindow(view: WebView?, isDialog: Boolean, isUserGesture: Boolean, resultMsg: Message?): Boolean {
+//
+//            webViewPopup = WebView(requireContext())
+//            webViewPopup?.isVerticalScrollBarEnabled = false
+//            webViewPopup?.isHorizontalScrollBarEnabled = false
+//            webViewPopup?.webViewClient = UriWebViewClient()
+//            webViewPopup?.settings?.javaScriptEnabled = true
+//            webViewPopup?.settings?.savePassword = false
+//            webViewPopup?.layoutParams = FrameLayout.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.MATCH_PARENT)
+//            web_view_popup_container?.addView(webViewPopup)
+//
+//            val transport: WebView.WebViewTransport = resultMsg?.obj as? WebView.WebViewTransport
+//                    ?: return false
+//
+//            transport.webView = webViewPopup
+//            resultMsg.sendToTarget()
+//
+//            return true
+//        }
+//
+//        override fun onCloseWindow(window: WebView?) {
+//            super.onCloseWindow(window)
+//            Log.d("Jozzee", "onCloseWindow")
+//        }
+//    }
 }
